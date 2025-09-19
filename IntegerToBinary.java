b7be82a94e35919c3152222392d15dc695b5626f
Sai Menta <prasanth.sai.menta@accenture.com>
2025-09-19 15:26:32 +0530

    Initial Code commit

---
+++ IntegerToBinary.java
@@ -0,0 +1,10 @@
+public class IntegerToBinary {
+
+    public static void main(String[] args) {
+
+        int x = 59;
+        String binaryVal = Integer.toBinaryString(x);
+        
+        System.out.println("Binary value is:" + binaryVal);
+    }
+}
