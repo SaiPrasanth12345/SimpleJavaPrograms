2b9108916b15e7795470042cd8c7dca5cdfd28e9
SaiPrasanth12345 <153602610+SaiPrasanth12345@users.noreply.github.com>
2025-11-23 01:04:20 +0530

    Update SortStringListByLength.java

--- SortStringListByLength.java
+++ SortStringListByLength.java
@@ -8,7 +8,8 @@ class Main {
         Collections.addAll(strList, "Thanks", "For", "Comming","to","Party");
         
         System.out.println(strList.size());
-        
+
+        // Sort List by length Using Comparator - based upon s1 & s2 lengths - in ascending order
         strList.sort((s1, s2) -> s1.length() - s2.length());
         
         strList.forEach(s -> System.out.println(s));
